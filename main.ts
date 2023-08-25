bfca18d946dfb4d82341f9c4b83dc8880220a5eb
Pierce Bartine <piercebartine@gmail.com>
2023-08-25 16:19:26 -0700

    Add WebService

--- main.ts
+++ main.ts
@@ -1,15 +1,18 @@
-import { Construct } from 'constructs';
-import { App, Chart, ChartProps } from 'cdk8s';
+import { Construct } from "constructs";
+import { App, Chart, ChartProps } from "cdk8s";
+import { WebService } from "./lib/web-service";
 
 export class MyChart extends Chart {
-  constructor(scope: Construct, id: string, props: ChartProps = { }) {
+  constructor(scope: Construct, id: string, props: ChartProps = {}) {
     super(scope, id, props);
 
     // define resources here
-
+    new WebService(this, "hello-k8s", {
+      image: "paulbouwer/hello-kubernetes:1.7",
+    });
   }
 }
 
 const app = new App();
-new MyChart(app, 'pbcloud-next');
+new MyChart(app, "hello");
 app.synth();
